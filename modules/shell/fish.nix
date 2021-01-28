0206ec988fbbbd2cc5ae3c52fa30aa3b69fb8e59
Ashkan Aleali <ashkan.aleali@gmail.com>
2021-01-28 00:39:33 -0600

    fix plugin hashes

--- modules/shell/fish.nix
+++ modules/shell/fish.nix
@@ -41,11 +41,25 @@ with lib; {
       # omf under the hood stuff
       {
         name = "omf-config";
-        src = fetchFromGitHub {
+        src = pkgs.fetchFromGitHub {
           owner = "oh-my-fish";
           repo = "plugin-config";
-          rev = "HEAD";
-          sha256 = "0x1rs89pmj5i8md2ihaw7dq62rz0qgkfky9vply5nx3immd66y6v";
+          rev = "13c424efb73b153d9b8ad92916cf51159d44099d";
+          sha256 = "23hjWq1xdFs8vTv56ebD4GdhcDtcwShaRbHIehPSOXQ=";
+          fetchSubmodules = true;
+        };
+      }
+
+      # https://github.com/lilyball/nix-env.fish
+      # nix-env
+      {
+        name = "nix-env";
+        src = fetchFromGitHub {
+          owner = "lilyball";
+          repo = "nix-env.fish";
+          rev = "a3c55307dce38c73485eac1f654c8f392341bda2";
+          sha256 = "7NDd7zrkYjTlvA5A1zhksZsc0aDNB4DVgmzRJHaxwIY=";
+          fetchSubmodules = true;
         };
       }
 
@@ -53,33 +67,36 @@ with lib; {
       # FIXME install https://github.com/holman/spark
       {
         name = "omf-spark";
-        src = fetchFromGitHub {
+        src = pkgs.fetchFromGitHub {
           owner = "oh-my-fish";
           repo = "plugin-spark";
-          rev = "HEAD";
-          sha256 = "0maglsr30ndhskwwysi8s6dy5pwhgifnhg63vlr00pw6yw33a160";
+          rev = "ad857daf57f6f450dd1cf5e8a5d63136bdbf7234";
+          sha256 = "uqUQ/O6euPSMIeIXgVuLTxvaB66w/V5xh9GWBQuXgTo=";
+          fetchSubmodules = true;
         };
       }
 
       # https://github.com/oh-my-fish/theme-bobthefish
       {
         name = "bobthefish";
-        src = fetchFromGitHub {
+        src = pkgs.fetchFromGitHub {
           owner = "oh-my-fish";
           repo = "theme-bobthefish";
-          rev = "HEAD";
-          sha256 = "gp8qYWg4o9l5H+0WiLf7GL+DGM20OvzKAsowouC6ax0=";
+          rev = "626bd39b002535d69e56adba5b58a1060cfb6d7b";
+          sha256 = "zUngqEZgHLmlyvoiVO3MwJTSFsYD7t3XiP6yMzmMkBs=";
+          fetchSubmodules = true;
         };
       }
 
       # https://github.com/oh-my-fish/plugin-foreign-env
       {
         name = "foreign-env";
-        src = fetchFromGitHub {
+        src = pkgs.fetchFromGitHub {
           owner = "oh-my-fish";
           repo = "plugin-foreign-env";
-          rev = "HEAD";
-          sha256 = "00xqlyl3lffc5l0viin1nyp819wf81fncqyz87jx8ljjdhilmgbs";
+          rev = "dddd9213272a0ab848d474d0cbde12ad034e65bc";
+          sha256 = "er1KI2xSUtTlQd9jZl1AjqeArrfBxrgBLcw5OqinuAM=";
+          fetchSubmodules = true;
         };
       }
 
@@ -87,11 +104,12 @@ with lib; {
       # FIXME ESCAPE not working
       {
         name = "sudope";
-        src = fetchFromGitHub {
+        src = pkgs.fetchFromGitHub {
           owner = "oh-my-fish";
           repo = "plugin-sudope";
-          rev = "HEAD";
-          sha256 = "1r6l79dzczl72gcqc4vwinvvjspzqvnjkrhhjkp54sb33f62281a";
+          rev = "bfb525e6d087ade031bf30a51f168ad07592f2df";
+          sha256 = "KiAhjBtjaVLulBDmKe3G/2q5t418E4bZE4d+9ls61OQ=";
+          fetchSubmodules = true;
         };
       }
 
@@ -100,101 +118,96 @@ with lib; {
       # FIXME alt doesn't work -- e.g. Alt-C not working
       {
         name = "fzf";
-        src = fetchFromGitHub {
+        src = pkgs.fetchFromGitHub {
           owner = "jethrokuan";
           repo = "fzf";
-          rev = "HEAD";
+          rev = "8ee7cf502637a9dd7d0cd96ead67c631a25e49d9";
           sha256 = "D20sR9H5lEh53nerhiuc5Ooid0AaD3Dcs85PgaNHFnk=";
+          fetchSubmodules = true;
         };
       }
 
       # https://github.com/oh-my-fish/plugin-osx
       {
         name = "osx";
-        src = fetchFromGitHub {
+        src = pkgs.fetchFromGitHub {
           owner = "oh-my-fish";
           repo = "plugin-osx";
-          rev = "HEAD";
-          sha256 = "032yfxz10vypywfivggsam77b8zplmgafbc0gqks8cxhfy9hh9cd";
+          rev = "27039b251201ec2e70d8e8052cbc59fa0ac3b3cd";
+          sha256 = "jSUIk3ewM6QnfoAtp16l96N1TlX6vR0d99dvEH53Xgw=";
+          fetchSubmodules = true;
         };
       }
 
       # https://github.com/oh-my-fish/plugin-thefuck
       {
         name = "thefuck";
-        src = fetchFromGitHub {
+        src = pkgs.fetchFromGitHub {
           owner = "oh-my-fish";
           repo = "plugin-thefuck";
-          rev = "HEAD";
-          sha256 = "0ih35vxvc05wc708i1ifr94s1p20k27bydqjlyk2gpv1cr8jf6j2";
+          rev = "3570c4464bdfd0f312e22a0b407a8c0ba98b7e0d";
+          sha256 = "QhonUWZh3yempxI3v46YQNygScouhojAYbwAtvsuA0Y=";
+          fetchSubmodules = true;
         };
       }
 
       # https://github.com/oh-my-fish/plugin-cd
       {
         name = "cd";
-        src = fetchFromGitHub {
+        src = pkgs.fetchFromGitHub {
           owner = "oh-my-fish";
           repo = "plugin-cd";
-          rev = "HEAD";
-          sha256 = "0q78d24wmkvn54c9blaylnjywr1562dgm2nc0vlb90b6n809vjmr";
+          rev = "c171d3bd1c68e5c22b87769497502c559de37aff";
+          sha256 = "ucqdALJmgbToBsyK+powJWTupaVe0ZUYKXbPyolo6GA=";
+          fetchSubmodules = true;
         };
       }
 
       # https://github.com/edc/bass
       {
         name = "bass";
-        src = fetchFromGitHub {
+        src = pkgs.fetchFromGitHub {
           owner = "edc";
           repo = "bass";
-          rev = "HEAD";
+          rev = "df4a1ebf8c0536e4bd7b7828a4c0dcb2b7b5d22b";
           sha256 = "VBqfBhHj0OyUmDzjak7OpSNxXlB0Xp1oG31To35u/rU=";
+          fetchSubmodules = true;
         };
       }
-
 
       # https://github.com/otms61/fish-pet
       {
         name = "pet";
-        src = fetchFromGitHub {
+        src = pkgs.fetchFromGitHub {
           owner = "otms61";
           repo = "fish-pet";
-          rev = "HEAD";
-          sha256 =
-            "sha256:0y8i3v2yp29r9m9cj5hyw6yd5jl1jzhq1ar4qlnd84kn80zahpsg";
+          rev = "c784c19b536471bd6ce403e2c5d06f17118362ca";
+          sha256 = "T1+oPkB2EtQsxSSrgOGXgcrSvOEeFslSTTmJ68UeEXk=";
+          fetchSubmodules = true;
         };
       }
 
       # https://github.com/joehillen/to-fish
       {
         name = "to";
-        src = fetchFromGitHub {
+        src = pkgs.fetchFromGitHub {
           owner = "joehillen";
           repo = "to-fish";
-          rev = "HEAD";
+          rev = "9e2ff18724212849bdf0f95a4f25b7b613013bb4";
           sha256 = "YGNsg4HhL/jz7eFLpZn9tzfRNriofwzbuhrY+rq9sIE=";
+          fetchSubmodules = true;
         };
       }
 
       # https://github.com/oh-my-fish/plugin-wifi-password
       {
         name = "wifi-password";
-        src = fetchFromGitHub {
+        src = pkgs.fetchFromGitHub {
           owner = "oh-my-fish";
           repo = "plugin-wifi-password";
-          rev = "HEAD";
-          sha256 = "1ayr4ircd1ys0gw0dfsagk4w3scfb3zbk32nk9648dzbsj7z7axn";
-        };
-      }
-
-      # https://github.com/oh-my-fish/plugin-python
-      {
-        name = "python";
-        src = fetchFromGitHub {
-          owner = "oh-my-fish";
-          repo = "plugin-python";
-          rev = "HEAD";
-          sha256 = "16nxd5w70rzrpxvlkn7xg592p6s1xh6v10anapfdm5p20azrkfnq";
+          rev = "7aea30350d64c62221b1371f9234aba039ab964b";
+          sha256 = "tqvzj9TrN0RMmlaMuf5YjunByXxKuwb4A9qHxnIk2as=";
+          fetchSubmodules = true;
         };
       }
     ];
