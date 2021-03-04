152537002d10f51f68fa9b1f95ad04068842d6aa
Ashkan Aleali <ashkan.aleali@gmail.com>
2021-03-03 22:13:51 -0600

    fix fish and tmux issues

--- modules/shell/fish.nix
+++ modules/shell/fish.nix
@@ -24,9 +24,9 @@ with lib; {
     shellAbbrs = import ./abbreviations.nix;
 
     # FIXME terrible hack
-    shellInit = ''
-      bass source /nix/store/lx21x6h12sqs00a2rfnkcjmd84shbjiy-set-environment
-    '';
+    # shellInit = ''
+    #   bass source /nix/store/lx21x6h12sqs00a2rfnkcjmd84shbjiy-set-environment
+    # '';
 
     # define the custom keybindings
     # interactiveShellInit = ''
