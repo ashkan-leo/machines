d0203675ac1478a59aff2d790d42a9d9fbbd340d
Ashkan Aleali <ashkan.aleali@gmail.com>
2021-05-22 17:37:32 -0500

    configure htop

--- modules/shell/misc.nix
+++ modules/shell/misc.nix
@@ -1,8 +1,48 @@
 { config, lib, pkgs, ... }:
 
 {
-  # activity monitoring
-  programs.htop.enable = true;
+  programs.htop = {
+    enable = true;
+    fields = [
+      "PID"
+      "USER"
+      "PRIORITY"
+      "M_RESIDENT"
+      "M_SIZE"
+      "PERCENT_CPU"
+      "PERCENT_MEM"
+      "TIME"
+      "COMM"
+    ];
+    meters = {
+      left = [
+        "LeftCPUs4"
+        "Memory"
+        "Blank"
+        "Uptime"
+      ];
+      right = [
+        "RightCPUs4"
+        "Swap"
+        "Blank"
+        "LoadAverage"
+      ];
+    };
+    sortKey = "PERCENT_CPU";
+    sortDescending = true;
+    hideKernelThreads = false;
+    showProgramPath = false;
+    highlightThreads = true;
+    highlightMegabytes = true;
+    treeView = false;
+    cpuCountFromZero = true;
+    showCpuFrequency = true;
+    showCpuUsage = true;
+    enableMouse = true;
+    colorScheme = 5;
+    vimMode = true;
+    delay = 20;
+  };
 
   # send notifications
   programs.noti.enable = true;
